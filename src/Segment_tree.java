74a0f310de79d01e64aceefe553d0c72bfe849b4
pashchenkoromak <pashchenkoromak@gmail.com>
2019-04-22 10:44:43 +0300

    qwert

--- src/Segment_tree.java
+++ src/Segment_tree.java
@@ -5,44 +5,31 @@ import java.util.*;
 import java.io.IOException;
 import java.io.File;
 import java.lang.Math.*;
+import static java.lang.Math.toIntExact;
 
 public class Segment_tree implements Serializable {
-    private ArrayList<Integer> t;
+    private long t[];
+    private Integer size;
     private Integer requestCount;
 
     public Segment_tree(String filepath) 
     {
-        t = new ArrayList();
         requestCount = new Integer(0);
 
         try {
             Scanner s = new Scanner(new File(filepath));
-            ArrayList<Integer> list = new ArrayList<Integer>(s.nextInt());
-            while (s.hasNext()){
-                list.add(s.nextInt());
-        }
+            size = s.nextInt();
+            t = new long[size];
+            for(int i = 0; i < size; i++)
+                t[i] = s.nextLong();
             s.close();
-            build(list, 1, 0, list.size() - 1);
         } catch (Exception e)
         {
             
         }
-        System.out.println(t.size());
+        System.out.println(size);
     }
     
-    private void build(ArrayList<Integer> a, Integer v, Integer tl, Integer tr)
-    {
-        System.out.println(a.size() + " " + v + " " + tl + " " + tr);
-        if (tl == tr)
-            t.add(v, a.get(tl));
-        else {
-            Integer tm = (tl + tr) / 2;
-            build (a, v*2, tl, tm);
-            build (a, v*2+1, tm+1, tr);
-            t.add(v, t.get(v*2) + t.get(v*2+1));
-        }
-    }
-
     public Integer getRequestCount()
     {
         return requestCount;
@@ -51,20 +38,14 @@ public class Segment_tree implements Serializable {
     public long getSum(long l, long r)
     {
         requestCount++;
-        return sum(1, 0, t.size() - 1, l, r);
+        return sum(1, 0, size - 1, l, r);
     }
 
     private long sum (Integer v, Integer tl, Integer tr, long l, long r) 
     {
-        if (v < 0)
-            return 0;
-        System.out.println(v + " " + tl + " " + tr + " " + l + " " + r);
-        if (l > r)
-            return 0;
-        if (l == tl && r == tr)
-            return t.get(v);
-        Integer tm = (tl + tr) / 2;
-        return sum (v*2, tl, tm, l, Math.min(r,tm))
-            + sum (v*2+1, tm+1, tr, Math.max(l,tm+1), r);
+        long ans = 0;
+        for(long i = l; i < r; i++)
+            ans += (t[(int)i]);
+        return ans;
     }
 }
